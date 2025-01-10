1e6a2ded974ca222270b691490fe34917b84e101
Tuncay Ekmez <tuncayekmez@gmail.com>
2025-01-10 16:13:26 +0300

    chore: Folder structure changes

--- types/video.ts
+++ types/video.ts
@@ -13,20 +13,20 @@ export const sampleVideos: Video[] = [
     title: 'What I ate today',
     description: 'Korean BBQ, sushi, hot pot and ice cream',
     date: '1 Ocak 2023',
-    videoUrl: require('../../assets/SampleVideo.mp4'),
+    videoUrl: require('../assets/SampleVideo.mp4'),
   },
   {
     id: '2',
     title: 'My New Year Resolutions',
     description: 'Read 30 books, travel to 5 new places, start a business',
     date: '31 Aralık 2022',
-    videoUrl: require('../../assets/SampleVideo.mp4'),
+    videoUrl: require('../assets/SampleVideo.mp4'),
   },
   {
     id: '3',
     title: 'My Favorite Things in 2022',
     description: 'I love my job, my friends and my life',
     date: '30 Aralık 2022',
-    videoUrl: require('../../assets/SampleVideo.mp4'),
+    videoUrl: require('../assets/SampleVideo.mp4'),
   }
 ]; 
