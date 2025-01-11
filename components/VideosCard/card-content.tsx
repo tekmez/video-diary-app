ed1a247653e6a17ca9f1370d334944c78eeaef8c
Tuncay Ekmez <tuncayekmez@gmail.com>
2025-01-11 17:04:12 +0300

    fix: Long desc issue at video card

--- components/VideosCard/card-content.tsx
+++ components/VideosCard/card-content.tsx
@@ -7,7 +7,11 @@ const CardContent = ({ video }: { video: Video }) => {
     <View className="flex-1 ml-4 justify-between">
       <View>
         <Text className="text-lg font-semibold">{video.title}</Text>
-        <Text className="text-gray-600 text-sm mt-1">{video.description}</Text>
+        <Text className="text-gray-600 text-sm mt-1">
+          {video.description.length > 120
+            ? `${video.description.slice(0, 85)}...`
+            : video.description}
+        </Text>
       </View>
       <Text className="text-gray-500 text-xs mt-2 text-right">
         {video.date}
